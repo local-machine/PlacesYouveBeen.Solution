84cc3e961b48b43d2e5457d2fc4d7fe12c82b51a
Student <example@example.com>
2019-07-31 14:26:39 -0700

    Added testing and confirm passing results

--- PlacesYouveBeen.Tests/ModelTests/PlacesTests.cs
+++ PlacesYouveBeen.Tests/ModelTests/PlacesTests.cs
@@ -6,104 +6,77 @@ using System.Collections.Generic;
 namespace PlacesYouveBeen.Test
 {
     [TestClass]
-    public class ItemTest : IDisposable
+    public class PlaceTest : IDisposable
     {
         public void Dispose()
         {
-            Item.ClearAll();
+            Place.ClearAll();
         }
         [TestMethod]
-        public void ItemConstructor_CreatesInstanceOfItem_Item()
+        public void PlaceConstructor_CreatesInstanceOfPlace_Place()
         {
-            Item newItem = new Item("test");
-            Assert.AreEqual(typeof(Item), newItem.GetType());
+            Place newPlace = new Place("test");
+            Assert.AreEqual(typeof(Place), newPlace.GetType());
         }
 
         [TestMethod]
-        public void GetDescription_ReturnsDescription_String()
-        {
-            //Arrange
-            string description = "Walk the dog.";
-            Item newItem = new Item(description);
-
-            //Act
-            string result = newItem.Description;
-
-            //Assert
-            Assert.AreEqual(description, result);
-        }
-        [TestMethod]
-        public void SetDescription_SetDescription_String()
+        public void GetCityName_ReturnsCityName_String()
         {
             //Arrange
-            string description = "Walk the dog.";
-            Item newItem = new Item(description);
+            string cityName = "Munich";
+            Place newPlace = new Place(cityName);
 
             //Act
-            string updatedDescription = "Do the dishes";
-            newItem.Description = updatedDescription;
-            string result = newItem.Description;
+            string result = newPlace.CityName;
 
             //Assert
-            Assert.AreEqual(updatedDescription, result);
+            Assert.AreEqual(cityName, result);
         }
+
         [TestMethod]
-        public void GetAll_ReturnsEmptyList_ItemList()
+        public void SetCityName_SetCityName_String()
+        {
+            //Arrange
+            string cityName = "Munich";
+            Place newPlace = new Place(cityName);
+
+            //Act
+            string updatedCityName = "Sao Paulo";
+            newPlace.CityName = updatedCityName;
+            string result = newPlace.CityName;
+
+            //Assert
+            Assert.AreEqual(updatedCityName, result);
+        }
+
+        [TestMethod]
+        public void GetAll_ReturnsEmptyList_PlaceList()
         {
             // Arrange
-            List<Item> newList = new List<Item> { };
+            List<Place> newList = new List<Place> { };
 
             //Act
-            List<Item> result = Item.GetAll();
+            List<Place> result = Place.GetAll();
 
             //Assert
             CollectionAssert.AreEqual(newList, result);
         }
+        
         [TestMethod]
-        public void GetAll_ReturnsItems_ItemList()
+        public void GetAll_ReturnsPlaces_PlaceList()
         {
             //Arrange
-            string description01 = "Walk the dog";
-            string description02 = "Wash the dishes";
-            Item newItem1 = new Item(description01);
-            Item newItem2 = new Item(description02);
-            List<Item> newList = new List<Item> { newItem1, newItem2 };
+            string cityName01 = "Munich";
+            string cityName02 = "Sao Paulo";
+            Place newPlace1 = new Place(cityName01);
+            Place newPlace2 = new Place(cityName02);
+            List<Place> newList = new List<Place> { newPlace1, newPlace2 };
 
             //Act
-            List<Item> result = Item.GetAll();
+            List<Place> result = Place.GetAll();
 
             //Assert
             CollectionAssert.AreEqual(newList, result);
-        }
-
-        [TestMethod]
-        public void GetId_ItemsInstantiateWithAnIdAndGetterReturns_Int()
-        {
-            // Arrange
-            string description = "Walk the dog.";
-            Item newItem = new Item(description);
-
-            // Act
-            int result = newItem.Id;
-
-            // Assert
-            Assert.AreEqual(1, result);
-        }
-
-        [TestMethod]
-        public void Find_ReturnsCorrectItem_Item()
-        {
-            // Arrange
-            string description01 = "Walk the dog";
-            string description02 = "Wash the dishes";
-            Item newItem1 = new Item(description01);
-            Item newItem2 = new Item(description02);
-
-            // Act
-            Item result = Item.Find(2);
-
-            // Assert
-            Assert.AreEqual(newItem2, result);
         }
     }
 }
--- PlacesYouveBeen/Models/Place.cs
+++ PlacesYouveBeen/Models/Place.cs
@@ -2,20 +2,18 @@ using System.Collections.Generic;
 
 namespace PlacesYouveBeen.Models
 {
-    public class Item
+    public class Place
     {
-        public string Description { get; set; }
-        public int Id { get; }
-        private static List<Item> _instances = new List<Item> {};
+        public string CityName { get; set; }
+        private static List<Place> _instances = new List<Place> {};
 
-        public Item (string description)
+        public Place (string cityName)
         {
-            Description = description;
+            CityName = cityName;
             _instances.Add(this);
-            Id = _instances.Count;
         }
 
-        public static List<Item> GetAll()
+        public static List<Place> GetAll()
         {
             return _instances;
         }
@@ -25,9 +23,5 @@ namespace PlacesYouveBeen.Models
             _instances.Clear();
         }
 
-        public static Item Find(int searchId)
-        {
-            return _instances[searchId - 1];
-        }
     }
 }
